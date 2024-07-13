ef2e352d136d5b5da3688d497df01ec83b254f19
Lesserkuma <lesserkuma4@gmail.com>
2024-07-13 20:49:25 +0200

    -

--- dl.sh
+++ dl.sh
@@ -8,5 +8,9 @@ mkdir -p /tmp/JoeyJr_FWUpdater
 unzip -qqjo /tmp/JoeyJr_FWUpdater.zip -d /tmp/JoeyJr_FWUpdater
 
 echo "Running the firmware updater..."
+sudo -nv >/dev/null 2>&1
+if [[ $? -ne 0 ]]; then
+    echo -e "This needs root privileges in order to access the raw sectors of your Joey Jr device."
+fi
 chmod +x /tmp/JoeyJr_FWUpdater/fwupdater.sh
 sudo /tmp/JoeyJr_FWUpdater/fwupdater.sh
